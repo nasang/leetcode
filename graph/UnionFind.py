d6379bc2812e0d592923b8b5b99ec513eb7a5694
Na Sang <cs.na.sang@gmail.com>
2024-11-28 20:31:44 -0800

    Update UnionFind.py

--- graph/UnionFind.py
+++ graph/UnionFind.py
@@ -1,16 +1,24 @@
-class UnionFind():
+class UnionFind:
     def __init__(self, n):
         self.parent = list(range(n))
+        self.rank = [1] * n
+        self.components_count = n
     
     def find(self, u):
         if self.parent[u] != u:
             self.parent[u] = self.find(self.parent[u])
-        return u
+        return self.parent[u]
     
     def union(self, u, v):
-        pu = self.find(u)
-        pv = self.find(v)
-        if pu == pv:
-            return False
+        pu, pv = self.find(u), self.find(v)
+        if pu == pv: return
         self.parent[pu] = pv
-        return True
+
+    def weightedQuickUnion(self, u, v):
+        pu, pv = self.find(u), self.find(v)
+        if pu == pv: return
+        if self.rank[pu] < self.rank[pv]:
+            pu, pv = pv, pu
+        self.parent[pv] = pu
+        self.rank[pu] += self.rank[pv]
+        self.components_count -= 1
